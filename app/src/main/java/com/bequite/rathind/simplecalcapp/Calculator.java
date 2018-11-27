61817e8610394e01610b467496e4db0c3d0ae210
Rathi <rathind14@gmail.com>
2018-11-27 11:16:57 -0500

    working simple calc

---
+++ app/src/main/java/com/bequite/rathind/simplecalcapp/Calculator.java
@@ -0,0 +1,37 @@
+package com.bequite.rathind.simplecalcapp;
+
+/**
+ * Utility Class for SimpleCalcApp to perform actual calculation
+ */
+
+public class Calculator {
+    public enum Operator {ADD, SUB, DIV, MUL}
+
+    /**
+     * Addition operation
+     */
+
+    public double add(double firstOperand, double secondOperand){
+        return firstOperand + secondOperand;
+    }
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+}
